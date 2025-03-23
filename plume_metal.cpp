383745a9c111b87122ae06aa90ec422d94d2c853
David Chavez <david@dcvz.io>
2025-03-23 18:18:11 +0100

    Fix Metal

--- plume_metal.cpp
+++ plume_metal.cpp
@@ -1935,11 +1935,6 @@ namespace plume {
 
     MetalCommandList::~MetalCommandList() {
         mtl->release();
-        indexBuffer->release();
-
-        for (MTL::Buffer *buffer : vertexBuffers) {
-            buffer->release();
-        }
     }
 
     void MetalCommandList::begin() {
@@ -2208,7 +2203,6 @@ namespace plume {
                 needsUpdate = i >= stateCache.lastVertexBuffers.size() || interfaceBuffer->mtl != stateCache.lastVertexBuffers[i] || newOffset != stateCache.lastVertexBufferOffsets[i] || newIndex != stateCache.lastVertexBufferIndices[i];
 
                 vertexBuffers[i] = interfaceBuffer->mtl;
-                vertexBuffers[i]->retain();
                 vertexBufferOffsets[i] = newOffset;
                 vertexBufferIndices[i] = newIndex;
             }
