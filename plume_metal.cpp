b320773d451a539ecd74de3b3f347042da53042c
David Chavez <david@dcvz.io>
2025-03-31 18:49:15 +0200

    Metal: Use default StorageMode for buffers

--- plume_metal.cpp
+++ plume_metal.cpp
@@ -1069,7 +1069,13 @@ namespace plume {
     }
 
     void MetalBuffer::unmap(uint32_t subresource, const RenderRange* writtenRange) {
-        // Do nothing.
+        if (mtl->storageMode() == MTL::StorageModeManaged) {
+            if (writtenRange == nullptr) {
+                mtl->didModifyRange(NS::Range(0, desc.size));
+            } else {
+                mtl->didModifyRange(NS::Range(writtenRange->begin, writtenRange->end - writtenRange->begin));
+            }
+        }
     }
 
     std::unique_ptr<RenderBufferFormattedView> MetalBuffer::createBufferFormattedView(RenderFormat format) {
@@ -1500,7 +1506,7 @@ namespace plume {
         requiredSize = alignUp(requiredSize, 256);
 
         argumentBuffer = {
-            .mtl = device->mtl->newBuffer(requiredSize, MTL::ResourceStorageModeManaged),
+            .mtl = device->mtl->newBuffer(requiredSize, MTL::ResourceStorageModeShared),
             .argumentEncoder = setLayout->argumentEncoder,
             .offset = 0,
         };
